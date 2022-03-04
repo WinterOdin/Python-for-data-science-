cbfff50684ef361ef80014892cb54d6b388c39ff
Marcel Czuryszkiewicz <45894369+WinterOdin@users.noreply.github.com>
2022-03-04 22:34:43 +0100

    Add files via upload

--- pandas - basics/dataframe.py
+++ pandas - basics/dataframe.py
@@ -26,8 +26,21 @@ df.drop('E', axis=0 ,inplace= True)
 row_less_than = df[df['Z']<0] 
 #get column X based on condition that there is a number in column W that is bigger than 0
 col_X = df[df['Y']>0][['Z','Z1']]
-
 #getting data with multiple contidions 
 #get only rows where column y and x is bigger than 0
 data_multiple = df[(df['Y']>0) & (df['X']>0)]
-print(data_multiple)
+#reseting index to be numerical needs to to be inplace=True if you wanna save the changes
+df.reset_index()
+#set new index from column 
+df['Z2'] = [10, 11, 12, 13]
+df.set_index('Z2', inplace=True)
+#multi index dataframe
+outside = ['G1', 'G1', 'G1', 'G2', 'G2', 'G2']
+inside = [1, 2, 3, 1, 2, 3]
+hier_index = list(zip(outside, inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+df1 = pd.DataFrame(randn(6,2), hier_index,['A','B'])
+#getting value form this df
+data1 = df1.loc["G2"].loc[2]['B']
+
+
